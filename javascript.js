0f3a7a5d363bfa85201e9cc9d74cb2dfab235b96
Ryan Ertel <122134901+ryanertel1@users.noreply.github.com>
2025-03-12 14:41:19 -0400

    Implemented ability to pull all necessary data out of API response and log it to console for now

--- javascript.js
+++ javascript.js
@@ -1,37 +1,6 @@
 // Giphy API call(replace query w/ search term): https://api.giphy.com/v1/gifs/search?api_key=DFDiQBA00oioIPYxJbBm8qm1Qlu4yAJd&q=query&limit=1&offset=0&rating=g&lang=en&bundle=messaging_non_clips
 // VC API call(replace Dayton w/ location variable): https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Dayton?unitGroup=us&elements=datetime%2Cname%2Ctemp%2Cicon%2Csource&include=fcst%2Cdays%2Ccurrent&key=5KWNMEZ5PF6YZM7PGMKJS94QL&contentType=json
 
-//requests weather data from visual crossing using submitted search query
-async function getWeather(location) {
-    var requestQuery = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + location + '?unitGroup=us&elements=datetime%2Cname%2Ctemp%2Cicon%2Csource&include=fcst%2Cdays%2Ccurrent&key=5KWNMEZ5PF6YZM7PGMKJS94QL&contentType=json'
-    try {
-        const response = await fetch(requestQuery, {mode: 'cors'});
-        const weatherData = await response.json();
-        console.log(weatherData);
-        localStorage.setItem("localWeatherData", JSON.stringify(weatherData)); //saves data from request into localstorage
-    } catch(error) { console.log(error) }
-}
-
-//retrieves data from prior request out of localstorage to save on API calls
-async function getWeatherFromFile() {
-    const data = localStorage.getItem("localWeatherData");
-    console.log('local data, not based on API request from search query')
-    console.log(JSON.parse(data));
-}
-
-function download(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
-  
-    element.style.display = 'none';
-    document.body.appendChild(element);
-  
-    element.click();
-  
-    document.body.removeChild(element);
-  }
-
 //verifies if the search query is valid before calling function that will make API request
 function verifySearch(searchLocation) {
     if (searchLocation && /^[a-zA-Z]*$/.test(searchLocation)){
@@ -39,6 +8,47 @@ function verifySearch(searchLocation) {
     }
     else { console.log('invalid search query') }
 }
+
+//requests weather data from visual crossing using submitted search query
+async function getWeather(location) {
+    var requestQuery = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + location + '?unitGroup=us&elements=datetime%2Cname%2Ctemp%2Cicon%2Csource&include=fcst%2Cdays%2Ccurrent&key=5KWNMEZ5PF6YZM7PGMKJS94QL&contentType=json'
+    try {
+        const response = await fetch(requestQuery, {mode: 'cors'});
+        const weatherData = await response.json();
+        localStorage.setItem("localWeatherData", JSON.stringify(weatherData)); //saves data from request into localstorage
+        setWeatherInfo(weatherData);
+    } catch(error) { console.log(error) }
+}
+
+//retrieves data from prior request out of localstorage to save on API calls
+function getWeatherFromFile() {
+    const storedData = localStorage.getItem("localWeatherData");
+    console.log('locally stored data used: not based on API request from search query')
+    weatherData = JSON.parse(storedData);
+    setWeatherInfo(weatherData)
+}
+
+function setWeatherInfo(weatherData) {
+    console.log(weatherData.address);
+    console.log('current time: ' + weatherData.currentConditions.datetime);
+    console.log(weatherData.currentConditions.temp);
+    selectIcon(weatherData.currentConditions.icon);
+
+    for (let i = 0; i <= numExtraDays; i++) {
+        let d = new Date(weatherData.days[i].datetime);
+        let day = d.getDay();
+        console.log(weeklyDays[day]);
+        console.log(weatherData.days[i].temp);
+        selectIcon(weatherData.days[i].icon);
+    }
+}
+
+function selectIcon(iconDescription) {
+    console.log(iconDescription)
+}
+
+const weeklyDays = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+const numExtraDays = 3;
 
 //Adds an event listener for form submission, prevents page reload, and grabs search query from text box
 window.addEventListener("DOMContentLoaded", (event) => {
